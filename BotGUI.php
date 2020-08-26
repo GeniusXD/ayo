a82066e1bde3af1e73e5f6fa8e29c15c27ec5bbd
Genius <56919066+GeniusXD@users.noreply.github.com>
2020-08-26 21:47:53 +0800

    Create BotGUI.php

---
+++ BotGUI.php
@@ -0,0 +1,45 @@
+<link rel="stylesheet" href="https://ex-script.com/fstyle/OhGG/iziToast.min.css">
+<script type="text/javascript" src="https://ex-script.com/fstyle/OhGG/iziToast.min.js"></script>
+<script>
+    //window.iziToast = iziToast;
+</script>
+
+<div id="mainone" style="position:fixed; min-width: 200px; z-index:9999; min-height: 100px; max-width: 900px; max-height: 200px;font-size:14px;">
+    <div id="botSector"
+        style="min-width: 25px;color:#fff; min-height: 25px;background: #333;max-width: 200px; max-height: 200px; border-radius: 10px">
+        <div id="botText"
+            style="margin-left: 10px;color:#fff0; width: 49px; height: 53; background: url('https://i.imgur.com/WZdqjIs.png') no-repeat;background-position-y: 1px;background-size: 45%">
+            _<span style="color: #fff; margin-left: 15px; ">Minions:</span><span style="color: #fff; margin-left: 5px;"
+                id="botCounter">0/0</span></div>
+    </div>
+    <div id="botSector"
+        style="display: none;min-width: 25px;color:#fff; min-height: 25px;background: #333;max-width: 200px; max-height: 200px; border-radius: 10px; margin-top: 5px">
+        <div id="botText"
+            style="margin-left: 10px;color:#fff0; width: 49px; height: 53; background: url('https://i.imgur.com/bIUuG5a.png') no-repeat;background-position-y: 1px;background-size: 45%">
+            _<span style="color: #fff; margin-left: 15px; ">Coins:</span><span style="color: #fff; margin-left: 5px;"
+                id="coinsCounter">0</span></div>
+    </div>
+    <div id="botSector"
+        style="min-width: 25px;color:#fff; min-height: 25px;background: #333;max-width: 200px; max-height: 200px; border-radius: 10px; margin-top: 5px">
+        <div id="botText"
+            style="margin-left: 10px;color:#fff0; width: 100%; background: url('https://i.imgur.com/F8B58GB.png') no-repeat;background-position-y: 1px;background-size: 11%">
+            _<span style="color: #fff; margin-left: 15px; ">Status:</span><span style="color: #fff; margin-left: 5px;"
+                id="serverStatus">Waiting</span></div>
+    </div>
+    <div id="userStatus"
+        style="min-width: 25px;color:#fff; min-height: 25px;background: #333;max-width: 200px; max-height: 200px; border-radius: 10px; margin-top: 5px">
+        <div id="botText"
+            style="margin-left: 10px;color:#fff0; width: 100%; background: url('https://i.imgur.com/H9UAQ5Q.png') no-repeat;background-position-y: 1px;background-size: 11%">
+            _<span style="color: #fff; margin-left: 15px; ">Users:</span><span
+                style="color: #fff; margin-left: 5px;" id="usersCounter">0</span></div>
+    </div>
+    <div id="UUIDSector"
+         style="text-align: center;min-width: 25px;color:#fff; min-height: 25px;background: #333;max-width: 900px; max-height: 200px; border-radius: 10px; margin-top: 5px">
+      <span style="color: #fff;">User token ( UUID )</span><span style="color: #fff; margin-left: -10px; display: block;" class="label label-info" id="UUID">hover to show</span>
+  </div>
+    <div id="UUIDSector"
+        style="text-align: center;min-width: 25px;color:#fff; min-height: 25px;background: #333;max-width: 900px; max-height: 200px; border-radius: 10px; margin-top: 5px">
+        200bots.ga<br><div id='toggleButton' class='btn btn-success'>Start
+            Bots</div><br></div>
+</div>
+</div>
